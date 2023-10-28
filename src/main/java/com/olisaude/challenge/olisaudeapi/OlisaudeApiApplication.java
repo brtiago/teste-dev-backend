afb8f5ccd0341ad12f3581df9f965b260b8ddad4
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-28 18:52:49 -0300

    Initial commit

---
+++ src/main/java/com/olisaude/challenge/olisaudeapi/OlisaudeApiApplication.java
@@ -0,0 +1,13 @@
+package com.olisaude.challenge.olisaudeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlisaudeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlisaudeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olisaude/challenge/olisaudeapi/OlisaudeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olisaude.challenge.olisaudeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlisaudeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
