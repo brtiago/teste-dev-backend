a4b0b43c1c55e3b449884717137c13a0a4321efc
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-30 19:32:55 -0300

    feat: adding CostumerGender enum

---
+++ src/main/java/com/olisaude/challenge/olisaudeapi/model/CostumerGender.java
@@ -0,0 +1,21 @@
+package com.olisaude.challenge.olisaudeapi.model;
+
+public enum CostumerGender {
+    MALE,
+    FEMALE,
+    TRANSGENDER,
+    NON_BINARY,
+    GENDERQUEER,
+    GENDERFLUID,
+    TWO_SPIRIT,
+    AGENDER,
+    BIGENDER,
+    ANDROGYNOUS,
+    CISGENDER,
+    DEMIGENDER,
+    NEUTROIS,
+    PANGENDER,
+    THIRD_GENDER,
+    INTERSEX,
+    OTHER
+}
