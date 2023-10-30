c720df2bf18b4680ba1a2d95bcad49e215bae332
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-30 19:33:14 -0300

    feat: adding CostumerRepository

---
+++ src/main/java/com/olisaude/challenge/olisaudeapi/repository/CostumerRepository.java
@@ -0,0 +1,7 @@
+package com.olisaude.challenge.olisaudeapi.repository;
+
+import com.olisaude.challenge.olisaudeapi.model.Costumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CostumerRepository extends JpaRepository<Costumer, Long> {
+}
