1652557d5a867a0d143f50d3cef4d6d857ddd3da
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-31 18:53:15 -0300

    feat: adding createCostumer method

--- src/main/java/com/olisaude/challenge/olisaudeapi/service/CostumerService.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/service/CostumerService.java
@@ -1,4 +1,18 @@
 package com.olisaude.challenge.olisaudeapi.service;
 
+import com.olisaude.challenge.olisaudeapi.dto.CostumerRequest;
+import com.olisaude.challenge.olisaudeapi.model.Costumer;
+import com.olisaude.challenge.olisaudeapi.repository.CostumerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CostumerService {
+    @Autowired
+    private CostumerRepository cr;
+    public Costumer createCostumer(CostumerRequest request) {
+        Costumer costumer = new Costumer(request);
+        System.out.println(costumer);
+        return cr.save(costumer);
+    }
 }
