4c64545f10ab8904ed032c40ed3093fe815d66f9
tiago <tgribeiro@gmail.com>
2023-11-01 17:05:47 -0300

    feat: add listall, delete and updatecostumer

--- src/main/java/com/olisaude/challenge/olisaudeapi/service/CostumerService.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/service/CostumerService.java
@@ -5,14 +5,36 @@ import com.olisaude.challenge.olisaudeapi.model.Costumer;
 import com.olisaude.challenge.olisaudeapi.repository.CostumerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class CostumerService {
     @Autowired
     private CostumerRepository cr;
-    public Costumer createCostumer(CostumerRequest request) {
+    public Costumer createCostumer(@RequestBody CostumerRequest request) {
         Costumer costumer = new Costumer(request);
         System.out.println(costumer);
         return cr.save(costumer);
     }
+
+    public List<Costumer> listAll(){
+        return cr.findAll();
+    }
+
+
+    public void delete(Long id) {
+        var costumer = cr.getReferenceById(id);
+        costumer.delete();
+    }
+
+    public Costumer updateCostumer(@RequestBody CostumerRequest request){
+        var costumer = cr.getReferenceById(request.id());
+        costumer.update(request);
+
+        return costumer;
+    }
+
+
 }
