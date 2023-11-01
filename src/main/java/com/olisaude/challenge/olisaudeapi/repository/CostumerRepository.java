d2f5169b3a11abb3c0855d54e9382e67409b3253
tiago <tgribeiro@gmail.com>
2023-11-01 17:04:22 -0300

    feat: adding findById and findByName method

--- src/main/java/com/olisaude/challenge/olisaudeapi/repository/CostumerRepository.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/repository/CostumerRepository.java
@@ -3,5 +3,9 @@ package com.olisaude.challenge.olisaudeapi.repository;
 import com.olisaude.challenge.olisaudeapi.model.Costumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CostumerRepository extends JpaRepository<Costumer, Long> {
+    Optional<Costumer> findById(Long id);
+    Optional<Costumer> findByName(String name);
 }
