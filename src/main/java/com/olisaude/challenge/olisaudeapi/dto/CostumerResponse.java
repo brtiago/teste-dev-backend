6d98bfd7c009c0d0c790e486af9900cf9f81434a
brtiago <727320+brtiago@users.noreply.github.com>
2023-11-07 18:14:22 -0300

    feat: add constructor

--- src/main/java/com/olisaude/challenge/olisaudeapi/dto/CostumerResponse.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/dto/CostumerResponse.java
@@ -11,16 +11,16 @@ public record CostumerResponse(
         String healthProblems,
         String degree
 ) {
-
-    public CostumerResponse(Costumer costumer) {
-        this(   costumer.getId(),
-                costumer.getName(),
-                String.valueOf(costumer.getBirthDate()),
-                costumer.getGender(),
-                costumer.getHealthProblem().getName(),
-                costumer.getHealthProblem().getDegree()
-        );
+    public CostumerResponse(Long id, String name, String birthDate, CostumerGender gender, String healthProblems, String degree) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.healthProblems = healthProblems;
+        this.degree = degree;
     }
 
-
+    public CostumerResponse(Costumer costumer) {
+        this(costumer.getId(), costumer.getName(), String.valueOf(costumer.getBirthDate()), costumer.getGender(), null, null);
+    }
 }
