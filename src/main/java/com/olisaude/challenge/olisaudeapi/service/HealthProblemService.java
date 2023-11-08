ccaabe47c3f3e9c835c638601ae0b9885bb900bc
brtiago <727320+brtiago@users.noreply.github.com>
2023-11-08 18:20:02 -0300

    refactor: create method moved from controller

--- src/main/java/com/olisaude/challenge/olisaudeapi/service/HealthProblemService.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/service/HealthProblemService.java
@@ -1,7 +1,24 @@
 package com.olisaude.challenge.olisaudeapi.service;
 
+import com.olisaude.challenge.olisaudeapi.dto.HealthProblemRequest;
+import com.olisaude.challenge.olisaudeapi.dto.HealthProblemResponse;
+import com.olisaude.challenge.olisaudeapi.model.HealthProblem;
+import com.olisaude.challenge.olisaudeapi.repository.HealthProblemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthProblemService {
+
+    @Autowired
+    private HealthProblemRepository repository;
+
+    public void create(HealthProblemRequest request) {
+        HealthProblem healthProblem = new HealthProblem(request);
+        repository.save(healthProblem);
+    }
+
+
 }
