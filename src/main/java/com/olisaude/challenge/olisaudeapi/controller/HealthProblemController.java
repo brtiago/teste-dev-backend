581acf8a140568f74ac69dfc663ffd12e5e34bef
tiago <tgribeiro@gmail.com>
2023-11-10 14:42:52 -0300

    refactor: refactoring update method to use service

--- src/main/java/com/olisaude/challenge/olisaudeapi/controller/HealthProblemController.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/controller/HealthProblemController.java
@@ -1,23 +1,16 @@
 package com.olisaude.challenge.olisaudeapi.controller;
 
-import com.olisaude.challenge.olisaudeapi.dto.CostumerResponse;
-import com.olisaude.challenge.olisaudeapi.dto.HealthProblemListResponse;
-import com.olisaude.challenge.olisaudeapi.dto.HealthProblemRequest;
-import com.olisaude.challenge.olisaudeapi.dto.HealthProblemResponse;
+import com.olisaude.challenge.olisaudeapi.dto.*;
 import com.olisaude.challenge.olisaudeapi.model.HealthProblem;
-import com.olisaude.challenge.olisaudeapi.repository.HealthProblemRepository;
 import com.olisaude.challenge.olisaudeapi.service.HealthProblemService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/healthproblem")
@@ -26,8 +19,6 @@ public class HealthProblemController {
     @Autowired
     private HealthProblemService service;
 
-    @Autowired
-    private HealthProblemRepository repository;
 
     @Transactional
     @PostMapping
@@ -40,7 +31,6 @@ public class HealthProblemController {
         }
     }
 
-
     @GetMapping
     public ResponseEntity<List<HealthProblemResponse>> listAll() {
         List<HealthProblemResponse> response = this.service.listAll();
@@ -49,10 +39,9 @@ public class HealthProblemController {
 
     @Transactional
     @PutMapping("/{id}")
-    public ResponseEntity<HealthProblem> update(@PathVariable Long id,@RequestBody HealthProblemRequest request) {
-        HealthProblem healthProblem = repository.getReferenceById(id);
-        healthProblem.update(request);
-        return ResponseEntity.ok(healthProblem);
+    public ResponseEntity<HealthProblem> update(@PathVariable Long id, @RequestBody HealthProblemUpdate dto) {
+        service.update(id, dto);
+        return ResponseEntity.ok().build();
     }
 
 }
