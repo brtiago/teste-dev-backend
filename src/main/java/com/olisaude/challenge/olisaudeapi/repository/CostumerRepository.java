16eb2b73646470668b7f757dd945ec09b5905368
brtiago <tgribeiro@gmail.com>
2023-11-13 17:44:27 -0300

    feat: creating findAllByActiveTrue

--- src/main/java/com/olisaude/challenge/olisaudeapi/repository/CostumerRepository.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/repository/CostumerRepository.java
@@ -3,9 +3,11 @@ package com.olisaude.challenge.olisaudeapi.repository;
 import com.olisaude.challenge.olisaudeapi.model.Costumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CostumerRepository extends JpaRepository<Costumer, Long> {
     Optional<Costumer> findById(Long id);
     Optional<Costumer> findByName(String name);
+    List<Costumer> findAllByActiveTrue();
 }
