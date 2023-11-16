a63179546097939dc5fb15021a2762afb76ba8fe
tiago <tgribeiro@gmail.com>
2023-11-16 15:40:45 -0300

    feat(database): add HealthScoreCalculator

--- src/main/java/com/olisaude/challenge/olisaudeapi/dto/CustomerResponse.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/dto/CustomerResponse.java
@@ -3,24 +3,31 @@ package com.olisaude.challenge.olisaudeapi.dto;
 import com.olisaude.challenge.olisaudeapi.model.Customer;
 import com.olisaude.challenge.olisaudeapi.model.CustomerGender;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public record CustomerResponse(
         Long id,
         String name,
         String birthDate,
         CustomerGender gender,
-        String healthProblems,
-        String degree
+        List<HealthProblemResponse> healthProblems
 ) {
-    public CustomerResponse(Long id, String name, String birthDate, CustomerGender gender, String healthProblems, String degree) {
+    public CustomerResponse(Long id, String name, String birthDate, CustomerGender gender, List<HealthProblemResponse> healthProblems) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
         this.gender = gender;
         this.healthProblems = healthProblems;
-        this.degree = degree;
     }
 
     public CustomerResponse(Customer customer) {
-        this(customer.getId(), customer.getName(), String.valueOf(customer.getBirthDate()), customer.getGender(), null, null);
+        this(customer.getId(),
+                customer.getName(),
+                String.valueOf(customer.getBirthDate()),
+                customer.getGender(),
+                customer.getHealthProblems().stream()
+                        .map(HealthProblemResponse::new)
+                        .collect(Collectors.toList()));
     }
 }
