a195d18ae4007d3afeb2d9d6a9d8282813fa8a5e
tiago <tgribeiro@gmail.com>
2023-11-17 16:21:08 -0300

    todo add a todo list

--- src/main/java/com/olisaude/challenge/olisaudeapi/service/CustomerService.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/service/CustomerService.java
@@ -1,7 +1,6 @@
 package com.olisaude.challenge.olisaudeapi.service;
 
 import com.olisaude.challenge.olisaudeapi.dto.CustomerRequest;
-import com.olisaude.challenge.olisaudeapi.dto.CustomerResponse;
 import com.olisaude.challenge.olisaudeapi.model.Customer;
 import com.olisaude.challenge.olisaudeapi.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
@@ -34,6 +32,11 @@ public class CustomerService {
 
     public Customer updateCustomer(Long id, CustomerRequest request){
         var customer = repository.getReferenceById(id);
+        //TODO: converter de HealthProblemRequest para HealthProblem usando o HealthProblemRepository
+        // List<HealthProblem> healthProblems = (...)
+        // Mandar no update junto com o request
+        // customer.update(request, healthProblems );
+
         customer.update(request);
 
         return customer;
