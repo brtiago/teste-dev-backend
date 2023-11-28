677368f382123772e84def5b5e87a16c8fae70c1
tiago <tgribeiro@gmail.com>
2023-11-28 16:41:30 -0300

    feat: implement findByName query

--- src/main/java/com/olisaude/challenge/olisaudeapi/repository/CustomerRepository.java
+++ src/main/java/com/olisaude/challenge/olisaudeapi/repository/CustomerRepository.java
@@ -2,13 +2,15 @@ package com.olisaude.challenge.olisaudeapi.repository;
 
 import com.olisaude.challenge.olisaudeapi.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findById(Long id);
-    Optional<Customer> findByName(String name);
+    @Query(value = "select c from Customer c where upper(trim(c.name)) like %?1%")
+    List<Customer> findByNameIgnoreCase(String name);
     List<Customer> findAllByActiveTrue();
 
     List<Customer> findTop10ByOrderByHealthScoreDesc();
